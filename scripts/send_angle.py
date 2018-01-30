fc706000e2db1189587d26ae857c3c657d881dda
YagishitaAkihiro <yagishitaakihiro@okadanet.org>
2018-01-30 14:54:01 +0900

    origin master

---
+++ scripts/send_angle.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import socket
+import time
+from contextlib import closing
+import rospy
+from std_msgs.msg import String
+
+global angle_data
+angle_data = ""
+
+def callback(data):
+    global angle_data
+    angle_data = data.data
+
+def main(host,port):
+  hosts = host
+  ports = port
+  count = 0
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  rospy.Subscriber("angle_data",String,callback,queue_size=1)
+  with closing(sock):
+    while not rospy.is_shutdown():
+      global angle_data
+#      print (angle_data)
+      sock.sendto(angle_data, (hosts, ports))
+      time.sleep(1)
+  return
+
+if __name__ == '__main__':
+  rospy.init_node("udp_send")
+  host =  '127.0.0.1'
+  port = 4000
+  main(host,port)
