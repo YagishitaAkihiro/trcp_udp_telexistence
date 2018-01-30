07e1a5e6e6ff9a3ac75523185374a751123a1b71
akihiro_yagishita <yagishitaakihiro@okadanet.org>
2018-01-30 15:28:20 +0900

    first comiit

---
+++ scripts/rec_angle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from nextage_ros_bridge import nextage_client
+from hrpsys import rtm
+from hironx_ros_bridge.ros_client import ROS_Client
+import argparse
+import rospy
+
+from __future__ import print_function
+import socket
+from contextlib import closing
+
+class Class(self,host,port,bufsize):
+      hosts = host
+      ports = port
+      bufsizes = bufsize
+      
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
+      with closing(sock):
+        sock.bind((hosts,ports))
+        while not rospy.is_shutdown():
+          print (sock.recv(bufsize))
+      return
+
+if __name__ == '__main__':
+#-------------------------------------------initial_setting------------------------------------------
+    parser = argparse.ArgumentParser(description='NEXTAGE Open command line interpreters')
+    parser.add_argument('--host', help='corba name server hostname')
+    parser.add_argument('--port', help='corba name server port number')
+    parser.add_argument('--modelfile', help='robot model file nmae')
+    parser.add_argument('--robot', help='robot modlule name (RobotHardware0 for real robot, Robot()')
+    args, unknown = parser.parse_known_args()
+  
+    if args.host:
+       rtm.nshost = args.host
+    if args.port:
+       rtm.nsport = args.port
+    if not args.robot:
+       args.robot = "RobotHardware0" if args.host else "HiroNX(Robot)0"
+    if not args.modelfile:
+       args.modelfile = ""
+  
+    if len(unknown) >= 2:
+       args.robot = unknown[0]
+       args.modelfile = unknown[1]
+    robot = nxc = nextage_client.NextageClient()
+    robot.init(robotname=args.robot, url=args.modelfile)
+    ros = ROS_Client()
+    
+    host = '127.0.0.1'
+    port = 4000
+    bufsize = 4096
+#--------------------------------------------end_initial_setting------------------------------------------
+#   主処理
+    Tele(host,port,bufsize)
