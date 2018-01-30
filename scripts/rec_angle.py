69e28991c6b2807a362769e2515f645ee36cb6d0
YagishitaAkihiro <yagishitaakihiro@okadanet.org>
2018-01-30 19:49:47 +0900

    update

--- scripts/rec_angle.py
+++ scripts/rec_angle.py
@@ -13,6 +13,15 @@ import rospy
 import socket
 from contextlib import closing
 
+def worker(angle_list):
+    if len(angle_list) == 24:
+       print (angle_list)
+#       joint = []
+#       robot.setJointAngles(angle_list,0.5)
+    else:
+       print ("error")
+       pass 
+
 def main(host,port,bufsize):
       hosts = host
       ports = port
@@ -22,11 +31,16 @@ def main(host,port,bufsize):
       with closing(sock):
         sock.bind((hosts,ports))
         while not rospy.is_shutdown():
-          print (sock.recv(bufsizes))
+          ang_list = sock.recv(bufsizes).replace('[','')
+          ang_list = ang_list.replace(']','')
+          ang_list = ang_list.split(",")
+#          print (ang_list)
+          worker(ang_list)
       return
 
 if __name__ == '__main__':
 #-------------------------------------------initial_setting------------------------------------------
+    """
     parser = argparse.ArgumentParser(description='NEXTAGE Open command line interpreters')
     parser.add_argument('--host', help='corba name server hostname')
     parser.add_argument('--port', help='corba name server port number')
@@ -49,8 +63,8 @@ if __name__ == '__main__':
     robot = nxc = nextage_client.NextageClient()
     robot.init(robotname=args.robot, url=args.modelfile)
     ros = ROS_Client()
-    
-    host = '10.254.21.23'
+    """
+    host = '10.254.21.24'
     port = 4000
     bufsize = 4096
 #--------------------------------------------end_initial_setting------------------------------------------
--- scripts/send_angle.py
+++ scripts/send_angle.py
@@ -11,7 +11,10 @@ angle_data = ""
 def callback(data):
     global angle_data
     angle_data = data.data
-
+#    a_d = a_d.replace('[', '')
+#    anlge_data = a_d#.replace(']', '')
+#    print (angle_data)
+    
 def main(host,port):
   hosts = host
   ports = port
@@ -28,6 +31,6 @@ def main(host,port):
 
 if __name__ == '__main__':
   rospy.init_node("udp_send")
-  host =  '127.0.0.1'
+  host = "10.254.21.24" #localhost'127.0.0.1'
   port = 4000
   main(host,port)
